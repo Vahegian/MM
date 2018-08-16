f6bba8dfe7f1b545489c471d64ed55c68ab4b919
gian <vahegian@gmail.com>
2018-08-16 20:19:01 +0100

    initial commit

--- src/mm/Main.java
+++ src/mm/Main.java
@@ -8,15 +8,6 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args){
-        String user= JOptionPane.showInputDialog("Input db user: ");
-        String pass = JOptionPane.showInputDialog("Input password: ");
-        String dbname= JOptionPane.showInputDialog("Input DB name: ");
-
-
-        DatabaseController database = new DatabaseController(dbname,user,pass);
-
-
-        new Frame(database);
-
+        new Frame();
     }
 }
