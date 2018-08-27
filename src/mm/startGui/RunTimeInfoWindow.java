9bbc7351dc70f5ab2cbff8458657e2fcb7fe1bb8
lavaguiny <vahegian@gmail.com>
2018-08-27 18:44:39 +0100

    Displays number of threads used in gui

---
+++ src/mm/startGui/RunTimeInfoWindow.java
@@ -0,0 +1,35 @@
+package mm.startGui;
+
+import mm.customObjects.Colors;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class RunTimeInfoWindow extends InfoFrame {
+    private final JPanel mainPanel;
+    private final JTextArea hint;
+    private final JTextArea value;
+
+    public RunTimeInfoWindow() {
+        super("Info", Toolkit.getDefaultToolkit().getScreenSize().width/2-120,Toolkit.getDefaultToolkit().getScreenSize().height-30, 240, 30);
+        mainPanel = new JPanel();
+        mainPanel.setBounds(0,0,sx,sy);
+        mainPanel.setBackground(Colors.white);
+        hint = new JTextArea();
+        hint.setBackground(Colors.white);
+        hint.setEditable(false);
+        value = new JTextArea();
+        value.setBackground(Colors.white);
+        value.setEditable(false);
+        mainPanel.add(hint);
+        mainPanel.add(value);
+        add(mainPanel);
+
+    }
+
+    public void setInfo(String hint, String value){
+        this.hint.setText(hint);
+        this.value.setText(value);
+        repaint();
+    }
+}
