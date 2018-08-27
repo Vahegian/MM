bf109e831298ab50861c86f37b6f1a49d45764de
lavaguiny <vahegian@gmail.com>
2018-08-27 17:13:47 +0100

    individual Threads are replaced with ExecutorService pool.

--- src/mm/Main.java
+++ src/mm/Main.java
@@ -7,9 +7,42 @@ import java.util.concurrent.Executors;
 
 public class Main {
     public static ExecutorService workers;
+    public static final int[] threadCount = {0};
     public static void main(String[] args){
 //      code is optimised for CPU with 2 Cores and 2.7GHz per core
         workers = Executors.newFixedThreadPool(10);
+        startCountingThreads();
         new Frame();
+    }
+
+    public static void plusThread(){
+        synchronized (Main.threadCount) {
+            Main.threadCount[0] += 1;
+        }
+    }
+
+    public static void minusThread(){
+        synchronized (Main.threadCount) {
+            Main.threadCount[0] -= 1;
+        }
+    }
+
+    private static void startCountingThreads(){
+        workers.execute(new Runnable() {
+            @Override
+            public void run() {
+                threadCount[0]+=1;
+                while (true){
+                    synchronized (threadCount) {
+                        System.err.println("Threads used from the pool: " + threadCount[0]);
+                    }
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
     }
 }
